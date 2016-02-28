f115ab48917ccb9f85d9acb9cf5f879a56a11438
ttmv <tmalmivi@cs.helsinki.fi>
2016-02-29 00:51:23 +0200

    tuhoamista

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
   has_many :attenders, through: :attendances, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, uniqueness: true
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
   has_many :events, through: :attendances
 end
