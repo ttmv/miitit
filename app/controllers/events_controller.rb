46ca1c6bc68360a97e8880ac2ec2a040c34d483f
ttmv <tmalmivi@cs.helsinki.fi>
2016-03-06 23:45:04 +0200

    paikan lisäys eventteihin

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -83,7 +83,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :time, :info)
+      params.require(:event).permit(:name, :time, :place, :info)
     end
     
     def get_attendance
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306205842) do
+ActiveRecord::Schema.define(version: 20160306214054) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "event_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160306205842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "time"
+    t.string   "place"
   end
 
   create_table "messages", force: :cascade do |t|
